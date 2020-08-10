c1662c1a6eaa3fdf2f2513c8cbae4378f2f95105
Jozef Bugos <jozef.bugos@sap.com>
2020-08-10 14:52:20 +0200

    trying to make selenium work

--- public/sandbox.py
+++ public/sandbox.py
@@ -21,7 +21,7 @@ own = data['own']
 
 options = Options()
 options.headless = True
-options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--example-flag')
 # options.add_argument("--example-flag")
 
 # driverLocation = '/usr/local/bin/chromedriver' #if windows
